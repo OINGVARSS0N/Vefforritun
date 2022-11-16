ba2c9a3687ac6e845eaffa3f92a854eafc3f7ac4
Óskar Ingvarsson <osi4@hi.is>
2022-11-16 12:39:38 +0000

    smalamo

--- v10/main.js
+++ v10/main.js
@@ -6,31 +6,58 @@ function route() {
   if (teikna) {
     document.querySelector('#app').innerHTML = `
     <div>
-    <p>wow svaka flottur blár kassi</p>
-      <canvas id="canvas"></canvas>
+    <h1>Hér fáum við að sjá svakalega flotta teikningu</h1>
+    <canvas id="canvas" width="1400" height="1000"></canvas>
+    <p>Höfundur er enginn annar en Óskar Pablo Diego José Francisco de Paula 
+    Juan Nepomuceno María de los Remedios Cipriano de la Santísima Trinidad Ruiz y Picasso</p>
     </div>
     `;
     const canvas = document.querySelector('canvas');
     const c = canvas.getContext('2d');
+    c.lineWidth = 5;
     c.strokeStyle = 'white';
-    c.fillStyle = 'blue';
-    c.rect(100, 20, 150, 100);
+
+    c.beginPath();
+    c.moveTo(400, 400);
+    c.lineTo(400, 300);
+    c.quadraticCurveTo(450, 250, 500, 300);
+    c.lineTo(500, 400);
     c.stroke();
-    c.fill();
+
+    c.beginPath();
+    c.moveTo(800, 400);
+    c.bezierCurveTo(800, 150, 1200, 700, 1200, 400);
+    c.stroke();
+    // Húsið okkar
+    c.lineWidth = 10;
+    c.strokeStyle = 'red';
     c.fillStyle = 'red';
-    c.fillRect(400, 500, 300, 250);
+    // Veggir
+    c.strokeRect(800, 500, 300, 200);
+    // Hurð
+    c.fillRect(925, 600, 50, 100);
+    // Þak
+    c.beginPath();
+    c.moveTo(700, 500);
+    c.lineTo(1200, 500);
+    c.lineTo(950, 300);
+    c.lineTo(700, 500);
+    c.stroke();
+    c.font = '30px Times-New-Roman';
+    c.fillText('Hello Zook,this could be ours but you playin', 100, 500);
   } else {
     document.querySelector('#app').innerHTML = `
     <h1>Welcome to your nightmare</h1>
     <div class="player">
   <video controls>
-    <source src="video/malao.mp4" type="video/mp4" />
+    <source src="/malao.mp4" type="video/mp4" />
   </video>
   </div>
 </div>
     <div>
         <a href="/?teikna=yes">
-        <div>Fyrirgefðu, ýta hér fyrir flotta teikningu</div>
+        <div>Fyrirgefðu, ýta hér fyrir flotta teikningu
+        </div>
       </a>
       </div>
     `;
